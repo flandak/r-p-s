a673b3db8f57905760b6b382f0132894b5918bc8
Jakub Flanderka <flandak@gmail.com>
2023-01-16 15:40:45 +0100

    added style

--- script.js
+++ script.js
@@ -1,101 +1,124 @@
 const choices = ["rock", "paper", "scissors"];
 const winners = [];
-const resetGame = () => {};
-// const game = () => {
-// };
-
-const startGame = () => {
-    //play until someone wins 5x
-    const imgs = document.querySelectorAll("img");
-    imgs.forEach((img) =>
-        img.addEventListener("click", () => {
-            if (img.id) {
-                playRound(img.id);
-            }
-        })
-    );
+const resetGame = () => {
+  winners = [];
+  document.querySelector(".playerScore").textContent = "Score: 0";
+  document.querySelector(".computerScore").textContent = "Score: 0";
+  document.querySelector(".ties").textContent = "Ties: 0";
+  document.querySelector(".winner").textContent = "";
+  document.querySelector(".playerChoice").textContent = "";
+  document.querySelector(".computerChoice").textContent = "";
+  document.querySelector(".reset").textContent = "none";
 };
 
-btn2.addEventListener("click", playRound);
-btn3.addEventListener("click", playRound);
-btn1.addEventListener("click", playRound);
+const startGame = () => {
+  //play until someone wins 5x
+  let imgs = document.querySelectorAll("img");
+  imgs.forEach((img) =>
+    img.addEventListener("click", () => {
+      if (img.id) {
+        playRound(img.id);
+      }
+    })
+  );
+};
 
 const playRound = (playerChoice) => {
   let wins = checkWins();
-  if (wins >= 5){
-    return
+  if (wins >= 5) {
+    return;
   }
-    // const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-    const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    // console.log(`Computer threw ${computerSelection}`);
-    // console.log(winner);
+  const computerChoice = computerSelect();
+  const winner = checkWinner(playerChoice, computerChoice);
+  winners.push(winner);
+  tallyWins();
+  displayRound(playerChoice, computerChoice, winner);
+  wins = checkWins();
+  if (wins == 5) {
+    //display end reults
+    //change the button to visible
+    //change the text to display winner
+    displayEnd();
+  }
+};
+const displayEnd = () => {
+  let playerWins = winners.filter((winner) => winner == "user").length;
+  if (playerWins == 5) {
+    document.querySelector(".winner").textContent = `You won 5 rounds!`;
+  } else {
+    document.querySelector(".winner").textContent = `Comp won 5 rounds HAHA`;
+  }
+  document.querySelector(".reset").style.display = "flex";
+};
+const displayRound = (playerChoice, computerChoice, winner) => {
+  document.querySelector(
+    ".playerChoice"
+  ).textContent = `You chose: ${playerChoice}`;
+  document.querySelector(
+    ".computerChoice"
+  ).textContent = `Comp chose: ${computerChoice}`;
+  displayRoundWinner(winner);
+};
+const displayRoundWinner = (winner) => {
+  if (winner == "user") {
+    document.querySelector(".winner").textContent = "You won the round";
+  } else if (winner == "computer") {
+    document.querySelector(".winner").textContent = "Comp won the round";
+  } else {
+    document.querySelector(".winner").textContent = "The round was a tie";
+  }
 };
 
-const computerChoice = () => {
-    const number = Math.floor(Math.random() * 3);
-    if (number === 0) {
-        return "rock";
-    } else if (number === 1) {
-        return "paper";
-    } else if (number === 2) {
-        return "scissors";
-    }
-}
-const checkWins = () =>{
-  const playerWins = winners.filter((winner) => winner == "you won!").length;
-  const computerWins = winners.filter((winner) => winner == "computer won HAHA").length;
-  return Math.max(playerWins,computerWins)
-}
-// let playerChoice = () => {
-//   let input = prompt("Type rock, paper or scissors");
-//   input = input.toLowerCase();
-//   while (input == null) {
-//     input = prompt("Type rock, paper or scissors");
-//   }
-//   let check = validateInput(input);
-//   while (check == false) {
-//     input = prompt(
-//       "type rock, paper or scissors exactly. You do not need to use capitalization"
-//     );
-//     while (input == null) {
-//       input = prompt("type rock, paper, or scissors");
-//     }
-//     input = input.toLowerCase();
-//     check = validateInput(input);
-//   }
-//   return input;
-// };
+const tallyWins = () => {
+  const playerWins = winners.filter((winner) => winner == "user").length;
+  const compWins = winners.filter((winner) => winner == "computer").length;
+  const ties = winners.filter((winner) => winner == "tie").length;
+  document.querySelector(".playerScore").textContent = `Score: ${playerWins}`;
+  document.querySelector(".computerScore").textContent = `Score: ${compWins}`;
+  document.querySelector(".ties").textContent = `Ties: ${ties}`;
+};
+
+const computerSelect = () => {
+  return choices[Math.floor(Math.random() * choices.length)];
+  //   const number = Math.floor(Math.random() * 3);
+  //   if (number === 0) {
+  //     return "rock";
+  //   } else if (number === 1) {
+  //     return "paper";
+  //   } else if (number === 2) {
+  //     return "scissors";
+  //   }
+};
+const checkWins = () => {
+  const playerWins = winners.filter((winner) => winner == "user").length;
+  const compWins = winners.filter((winner) => winner == "computer").length;
+  return Math.max(playerWins, compWins);
+};
 
 const validateInput = (choice) => {
-    if (choices.includes(choice)) {
-        return true;
-    } else {
-        return false;
-    }
+  if (choices.includes(choice)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const checkWinner = (choiceP, choiceC) => {
-    if (choiceP === choiceC) {
-        return "The game ended in a tie";
-    } else if (
-        (choiceP === "rock" && choiceC === "scissors") ||
-        (choiceP === "paper" && choiceC === "rock") ||
-        (choiceP === "scissors" && choiceC === "rock")
-    ) {
-        return "you won!";
-    } else {
-        return "computer won HAHA";
-    }
+  if (
+    (choiceP === "rock" && choiceC === "scissors") ||
+    (choiceP === "paper" && choiceC === "rock") ||
+    (choiceP === "scissors" && choiceC === "paper")
+  ) {
+    return "user";
+  } else if (choiceP == choiceC) {
+    return "tie";
+  } else {
+    return "computer";
+  }
 };
 const logWins = () => {
-    const playerWins = winners.filter((winner) => winner == "you won!").length;
-    const computerWins = winners.filter(
-        (winner) => winner == "computer won HAHA"
-    ).length;
-    const ties = winners.filter(
-        (winner) => winner == "The game ended in a tie"
-    ).length;
+  const playerWins = winners.filter((winner) => winner == "user").length;
+  const compWins = winners.filter((winner) => winner == "computer").length;
+  const ties = winners.filter((winner) => winner == "tie").length;
 };
 startGame();
